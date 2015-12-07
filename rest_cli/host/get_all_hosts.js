13c940ce5bbaec0e634344b05104d627a1e7da69
Tim Goeke <tim.goeke@gmail.com>
2015-12-06 20:49:46 -0700

    Added get all hosts.

--- rest_cli/host/get_all_hosts.js
+++ rest_cli/host/get_all_hosts.js
@@ -1,24 +1,16 @@
 var request = require('request');
 
-if( process.argv.length != 6 ) {
-    console.log( "Usage: create_host restHost dockerHost sshUser sshPassword" );
+if( process.argv.length != 3 ) {
+    console.log( "Usage: get_all_hosts restHost" );
 } else {
 
     var restHost = process.argv[2];
-    var dockerHost = process.argv[3];
-    var sshUser = process.argv[4];
-    var sshPassword = process.argv[5];
 
-    console.log( "Creating new host for: " + dockerHost + " on " + restHost );
+    console.log( "Getting all docker hosts on: " + restHost );
 
     var options = {
         uri: 'https://' + restHost + ':8443/host',
-        method: 'POST',
-        json: {
-            "hostname": dockerHost,
-            "sshUser": sshUser,
-            "sshPassword": sshPassword
-        },
+        method: 'GET',
         headers: {
             'Authorization': 'Basic ' + new Buffer('admin:Awesome123!').toString('base64')
         },
