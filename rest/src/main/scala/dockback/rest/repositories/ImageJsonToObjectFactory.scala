acfb857f16b57be5ad13c49c40f1a46154a8820f
Tim Goeke <tim.goeke@gmail.com>
2016-01-12 06:52:18 -0700

    First stubs for image json parsing.

---
+++ rest/src/main/scala/dockback/rest/repositories/ImageJsonToObjectFactory.scala
@@ -0,0 +1,26 @@
+package dockback.rest.repositories
+
+import java.util
+
+import dockback.domain.Image
+
+import play.api.libs.json._ // JSON library
+import play.api.libs.json.Reads._ // Custom validation helpers
+import play.api.libs.functional.syntax._ // Combinator syntax
+
+object ImageJsonToObjectFactory {
+
+  def parseImage( imageJson: String ) : Image = {
+
+    JsPath
+
+    Image("", "", "", Array(""), 0L)
+
+  }
+
+  def parseImages( imagesJson: String ) : java.util.List[Image] = {
+
+    new util.ArrayList[Image]()
+
+  }
+}
