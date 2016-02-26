49bae492c2543ef218954c2cfcb7c39754d80d57
Tim Goeke <tim.goeke@gmail.com>
2016-02-25 22:18:31 -0700

    More domain updates.

--- domain/src/main/scala/dockback/domain/DockerHostConfig.scala
+++ domain/src/main/scala/dockback/domain/DockerHostConfig.scala
@@ -1,3 +1,27 @@
 package dockback.domain
 
-case class DockerHostConfig()
+case class DockerHostConfig(
+  binds: Array[String],
+  containerIdFile: String,
+  lxcConf: Array[String],
+  memory: Long,
+  memoryReservation: Long,
+  memorySwap: Long,
+  kernelMemory: Long,
+  cpuShares: Long,
+  cpuPeriod: Long,
+  cpusetCpus: String,
+  cpusetMem: String,
+  cpuQuota: Long,
+  blkioWeight: Long,
+  oomKillDisable: Boolean,
+  memorySwappiness: Int,
+  privileged: Boolean,
+  portBindings: Array[Int],
+  links: Array[String],
+  publishAllPorts: Boolean,
+  dns: Array[String],
+  dnsOptions: Array[String],
+  dnsSearch: Array[String],
+  extraHosts: Array[String]
+)
