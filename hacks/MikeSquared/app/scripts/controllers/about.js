dfc01a5b1028f41275dd3562252ee21edf993aa8
Michael S. Brown <michaelsbrown@MikesAir.local>
2016-03-03 12:48:46 -0700

    Pushing in my hack for Angular work for the UI

---
+++ hacks/MikeSquared/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fishboneApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fishboneApp
+ */
+angular.module('fishboneApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
